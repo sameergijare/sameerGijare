afa598f8b35159e5d195289d340bb75110f4ff6b
sameergijare <31246148+sameergijare@users.noreply.github.com>
2026-06-04 18:31:55 +0100

    Delete project2/run.bat

--- project2/run.bat
+++
@@ -1,2 +0,0 @@
-python model_classifyAppRisk.py "../input/raw" "../config/feature_name_to_number_mapping.csv" "../outputs/models"
-python test.py "../input/test" "../config/feature_name_to_number_mapping.csv" "../outputs/models/malware_model.pkl"
